2e87782b12baf224edc7e941e1e91563f4e5949b
Zhang Jingqiang <zh_jq@outlook.com>
2026-04-11 23:20:04 +0800

    add ktls feature

--- src/lib.rs
+++ src/lib.rs
@@ -194,6 +194,10 @@ impl Build {
             // Avoid multilib-postfix for build targets that specify it
             .arg("--libdir=lib");
 
+        if cfg!(feature = "ktls") {
+            configure.arg("enable-ktls");
+        }
+
         if target.contains("musl") {
             // Engine module fails to compile on musl (it needs linux/version.h
             // right now) but we don't actually need this most of the time.
